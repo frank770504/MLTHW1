be4bdb6ea0e818b5bbad5ae9f56ebe0847ab4e81
Frank <frank770504@gmail.com>
2015-01-09 01:08:11 +0800

    solve the g formula

--- QP/QPSolverInplement.m
+++ QP/QPSolverInplement.m
@@ -44,4 +44,43 @@ alpha = quadprog(H,f,[],[],Aeq,beq,lb,[],[],optimset('Algorithm','interior-point
 fprintf('max(alpha) is %d-th\n', find(max(alpha)==alpha));
 fprintf('min(alpha) is %d-th\n', find(min(alpha)==alpha));
 fprintf('sum(alpha) is %f\n', sum(alpha));
-  
+
+%find the g(x)
+%         N
+%        ----
+% g(x) =  >    alpha_n*y_n*K(x_n,x) + b
+%        ----
+%        n = 1
+%               N
+%              ----
+%    b = y_s -  >    alpha_n*y_n*K(x_n,x_s)
+%              ----
+%              n = 1
+[v ind] = max(alpha);
+y = X(:,3);
+b = y(ind);
+xs = X(ind,1:2)';
+for i=1:N,
+    xi = X(i,1:2)';
+    b = b - alpha(i)*y(i)*plyKernel(xi,xs);
+end
+%m = [ 1 2xn1 2xn2 xn1^2 xn2^2 2x1x2]
+%      1 x1   x2   x1^2  x2^2  x1x2
+M = [];
+for i=1:n,
+    A = alpha(i)*y(i);
+    xn1 = X(i,1);
+    xn2 = X(i,2);
+    m = [1 2*xn1 2*xn2 xn1^2 xn2^2 2*xn1*xn2];
+    M = [M;A.*m];
+end
+M = sum(M);
+
+syms x1 real 
+syms x2 real
+M = sym(M, 'd')';
+XX = [ 1;x1;x2;x1^2;x2^2;x1*x2 ];
+%g(x) = XX'*M + b
+g = XX'*M + b;
+fprintf('the g(x) is');
+pretty(g)
